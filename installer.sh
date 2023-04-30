5497857ed84093b28d1cc6b0008f1cfb8ef5975e
Aamir Syed <asyed94@gmail.com>
2023-04-29 22:16:07 -0500

    Checks for existence before using saved values

--- installer.sh
+++ installer.sh
@@ -139,26 +139,26 @@ if [ ! -z "${CONTAINER_ID}" ]; then
   docker-safe stop "${CONTAINER_ID}"
   docker-safe rm "${CONTAINER_ID}"
 
-  # UPDATE DEFAULT VALUES WITH SAVED VALUES
-  DASHPORT_DEFAULT=$(echo $ENV_VARS | grep -oP 'DASHPORT=\K[^ ]+')
-  EXTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'EXT_IP=\K[^ ]+')
-  INTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'INT_IP=\K[^ ]+')
-  SHMEXT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMEXT=\K[^ ]+')
-  SHMINT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMINT=\K[^ ]+')
-  PREVIOUS_PASSWORD=$(echo $ENV_VARS | grep -oP 'DASHPASS=\K[^ ]+')
+  # UPDATE DEFAULT VALUES WITH SAVED VALUES, IF THEY EXIST
+  grep -oP 'DASHPORT=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && DASHPORT_DEFAULT=$(echo $ENV_VARS | grep -oP 'DASHPORT=\K[^ ]+')
+  grep -oP 'EXT_IP=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && EXTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'EXT_IP=\K[^ ]+')
+  grep -oP 'INT_IP=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && INTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'INT_IP=\K[^ ]+')
+  grep -oP 'SHMEXT=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && SHMEXT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMEXT=\K[^ ]+')
+  grep -oP 'SHMINT=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && SHMINT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMINT=\K[^ ]+')
+  grep -oP 'PREVIOUS_PASSWORD=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && PREVIOUS_PASSWORD=$(echo $ENV_VARS | grep -oP 'DASHPASS=\K[^ ]+')
 elif [ -f .shardeum/.env ]; then
   echo "Existing .shardeum/.env file found. Reading settings from file."
 
   # Read the .shardeum/.env file into a variable. Use default installer directory if it exists.
   ENV_VARS=$(cat .shardeum/.env)
 
-  # UPDATE DEFAULT VALUES WITH SAVED VALUES
-  DASHPORT_DEFAULT=$(echo $ENV_VARS | grep -oP 'DASHPORT=\K[^ ]+')
-  EXTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'EXT_IP=\K[^ ]+')
-  INTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'INT_IP=\K[^ ]+')
-  SHMEXT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMEXT=\K[^ ]+')
-  SHMINT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMINT=\K[^ ]+')
-  PREVIOUS_PASSWORD=$(echo $ENV_VARS | grep -oP 'DASHPASS=\K[^ ]+')
+  # UPDATE DEFAULT VALUES WITH SAVED VALUES, IF THEY EXIST
+  grep -oP 'DASHPORT=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && DASHPORT_DEFAULT=$(echo $ENV_VARS | grep -oP 'DASHPORT=\K[^ ]+')
+  grep -oP 'EXT_IP=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && EXTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'EXT_IP=\K[^ ]+')
+  grep -oP 'INT_IP=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && INTERNALIP_DEFAULT=$(echo $ENV_VARS | grep -oP 'INT_IP=\K[^ ]+')
+  grep -oP 'SHMEXT=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && SHMEXT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMEXT=\K[^ ]+')
+  grep -oP 'SHMINT=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && SHMINT_DEFAULT=$(echo $ENV_VARS | grep -oP 'SHMINT=\K[^ ]+')
+  grep -oP 'PREVIOUS_PASSWORD=\K[^ ]+' <<< "$ENV_VARS" > /dev/null && PREVIOUS_PASSWORD=$(echo $ENV_VARS | grep -oP 'DASHPASS=\K[^ ]+')
 fi
 
 cat << EOF
